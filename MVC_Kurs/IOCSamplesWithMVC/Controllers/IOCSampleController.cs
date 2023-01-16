8498ab780551bfcd06779461b7b2075e623adc5e
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2023-01-16 12:02:48 +0100

    MVC - IOC Samples

---
+++ MVC_Kurs/IOCSamplesWithMVC/Controllers/IOCSampleController.cs
@@ -0,0 +1,45 @@
+﻿using IOCSamplesWithMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IOCSamplesWithMVC.Controllers
+{
+    //Wie greife ich auf den IOC-Container via Controller-Klasse zu?
+
+    public class IOCSampleController : Controller
+    {
+        private ITimeService klassenweiterTimeService; //ÜBerall von anderen Methoden dieser Klasse verwendbar
+
+        //Variante 1 -> Konstruktor Injections
+        public IOCSampleController(ITimeService timeService) 
+        {
+            klassenweiterTimeService = timeService;
+        }
+
+
+
+        //Variante 2: Methoden  - Injection -> Wenn ein Dienst oder eine Funktionalität nur innerhalb einer Methode verwendet wird
+        public IActionResult Index([FromServices] ITimeService methodenWeiterTimeService)
+        {
+            string currentTime = methodenWeiterTimeService.GetObjectInstanceTime();
+
+            return View();
+        }
+
+
+        public IActionResult Index2()
+        {
+            //Via HttpContext - GetService
+            ITimeService service1 = this.HttpContext.RequestServices.GetService<ITimeService>();
+
+
+            //oder 
+
+
+            //Via HttpContext - GetRequiredService
+            ITimeService service2 = this.HttpContext.RequestServices.GetRequiredService<ITimeService>();
+
+
+            return View();
+        }
+    }
+}
--- MVC_Kurs/IOCSamplesWithMVC/Program.cs
+++ MVC_Kurs/IOCSamplesWithMVC/Program.cs
@@ -1,3 +1,5 @@
+using IOCSamplesWithMVC.Services;
+
 namespace IOCSamplesWithMVC
 {
     public class Program
@@ -6,11 +8,26 @@ namespace IOCSamplesWithMVC
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddSingleton<ITimeService, TimeService>();
+
+
             // Add services to the container
             // //builder.Services -> IServiceCollection 
             builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+            var app = builder.Build(); //Initialsierung des IOC Containers ist hiermit beendet
+
+            ITimeService service1 = app.Services.GetService<ITimeService>();
+            ITimeService service2 = app.Services.GetRequiredService<ITimeService>();
+
+
+            //ab .NET 2.1 
+            using (IServiceScope scope = app.Services.CreateScope())
+            {
+                ITimeService service3 = scope.ServiceProvider.GetService<ITimeService>();
+                ITimeService service4 = scope.ServiceProvider.GetRequiredService<ITimeService>();
+            }
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
